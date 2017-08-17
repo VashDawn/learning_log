b53608a02add300f78597a1c461e0ff3ea6801b3
vash <vash.d@outlook.com>
2017-08-17 14:35:33 +0800

    some fixes in learning_logs/models

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Topic(models.Model):
     """User learning topic."""
     text = models.CharField(max_length=200)
-    data_added = models.DateTimeField(auto_now_add=True)
+    date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         """返回模型的字符串表示"""
