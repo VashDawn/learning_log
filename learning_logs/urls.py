d7b4ec270d784c373af76d5de3628b56d90efaf5
vash <vash.d@outlook.com>
2017-08-23 17:01:10 +0800

    ...

---
+++ learning_logs/urls.py
@@ -0,0 +1,16 @@
+"""Define urlpatterns of 'learning_logs'."""
+
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns=[
+    # main page
+    url(r'^$', views.index, name='index'),
+
+    # show all topics
+    url(r'^topics/$', views.topics, name='topics'),
+
+    # show single topic and all of it's entries.
+    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
+]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from learning_logs.models import Topic
+
+
+def index(request):
+    """Main page of learning_log."""
+    return render(request, 'learning_logs/index.html')
+
+
+def topics(request):
+    """Show all topics."""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+
+def topic(request, topic_id):
+    """Show single topic and all of it's entries."""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
