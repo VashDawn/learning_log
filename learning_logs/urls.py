4cc9461b79b0461eb6b947f8ab715d5581939204
vash <vash.d@outlook.com>
2017-08-25 16:24:37 +0800

    Add new_entry page.

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -15,5 +15,8 @@ urlpatterns=[
     url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
 
     # page for adding new topic
-    url(r'^new_topic/$', views.new_topic, name='new_topic')
+    url(r'^new_topic/$', views.new_topic, name='new_topic'),
+
+    # page for creating new entry
+    url(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry, name='new_entry')
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 
 # Create your views here.
 
@@ -34,7 +34,7 @@ def new_topic(request):
         # No data, create a new form.
         form = TopicForm()
     else:
-        # POST data, prodess data.
+        # POST data, prodess with the data.
         form = TopicForm(request.POST)
         if form.is_valid():
             form.save()
@@ -42,3 +42,24 @@ def new_topic(request):
 
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
+
+
+def new_entry(request, topic_id):
+    """Add new entry to a specified topic."""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        # no data, create a empty form
+        form = EntryForm()
+    else:
+        # POST data, process with the data
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic',
+                                                args=[topic_id]))
+
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
